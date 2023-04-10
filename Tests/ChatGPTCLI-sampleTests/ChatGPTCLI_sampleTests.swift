2725be136d65cf232524ac97f0f8e645454de37e
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2023-04-10 09:58:39 +0900

    Add: argument control

--- Tests/ChatGPTCLI-sampleTests/ChatGPTCLI_sampleTests.swift
+++ Tests/ChatGPTCLI-sampleTests/ChatGPTCLI_sampleTests.swift
@@ -1,11 +1,9 @@
 import XCTest
-@testable import ChatGPTCLI_sample
 
 final class ChatGPTCLI_sampleTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ChatGPTCLI_sample().text, "Hello, World!")
     }
 }
