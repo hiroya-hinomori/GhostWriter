7e8128b43749c4e0c8a041eb65515ec744487178
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2023-04-12 12:50:57 +0900

    Add: Sample files for Mapper

---
+++ Samples/Source.swift
@@ -0,0 +1,15 @@
+//
+//  Source.swift
+//  
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2023/04/12.
+//
+
+import Foundation
+
+struct Source: Decodable {
+    let title: String
+    let index: Int
+    let flag: Bool
+    let url: String
+}
