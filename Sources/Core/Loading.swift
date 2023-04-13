9a73ec194bb6b1a9f98cb691d253f338d4ba40fc
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2023-04-13 16:24:22 +0900

    Add: loading

---
+++ Sources/Core/Loading.swift
@@ -0,0 +1,33 @@
+//
+//  Loading.swift
+//  Core
+//
+//  Created by 日野森 寛也（Hiroya Hinomori） on 2023/04/13.
+//
+
+import Foundation
+
+class Loading {
+    var isRunning = true
+    var count = 0
+    
+    func show() async {
+        print("Now loading... Please wait for a few minutes.")
+        while isRunning {
+            try? await Task.sleep(for: .seconds(1))
+            count = resetIfNeeded(increment(count))
+            print(String(repeating:"🏎", count:count))
+        }
+    }
+    
+    func increment(_ value: Int) -> Int {
+        value + 1
+    }
+    
+    func resetIfNeeded(_ value: Int) -> Int {
+        if value % 11 == 0 {
+            return 0
+        }
+        return value
+    }
+}
